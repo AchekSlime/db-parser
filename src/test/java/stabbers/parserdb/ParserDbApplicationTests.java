28d97bea17300fad99ec3d033465c43e8262dab5
Achek <achek@MacBook-Pro-Achek.local>
2021-08-16 03:22:03 +0300

    Initial commit

---
+++ src/test/java/stabbers/parserdb/ParserDbApplicationTests.java
@@ -0,0 +1,13 @@
+package stabbers.parserdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
